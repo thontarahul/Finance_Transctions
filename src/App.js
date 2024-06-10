40ae30b945385bcc581ee1689de664067bb3cb91
Poojitha032715 <poojithapooja8220@gmail.com>
2024-06-10 09:51:42 +0530

    css

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home/*" element={<HomePage />} />
+        <Route path="/home/*" element={<HomePage />} /> 
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/logoutPage" element={<LogoutPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
